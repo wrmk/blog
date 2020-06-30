61c7ae647c116f005f33934a72ef1ee6b43a3198
wrmk <elmakan@gmail.com>
2020-06-30 21:20:10 +0300

    save comments to db

--- app.rb
+++ app.rb
@@ -72,5 +72,6 @@ end
 post '/details/:post_id' do
 	post_id = params[:post_id]
 	content = params[:content]
-	erb "you type #{content} for post #{post_id}"
+	@db.execute 'INSERT INTO Comments (content, created_date, post_id) values (?,datetime(), ?)', [content, post_id]
+	redirect to('/details/' + post_id)
 end	
